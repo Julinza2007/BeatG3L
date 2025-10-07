ebc816db9bb93654eed5f8ee7600f12b9bf13d1d
Eluney Laje <eluneylaje@gmail.com>
2025-10-07 14:34:25 -0300

    se arreglo la forma en la que baja la nota

--- src/GUI/Cancion1.java
+++ src/GUI/Cancion1.java
@@ -25,26 +25,32 @@ public class Cancion1 extends CancionBase {
     protected void construirCancion(ResolucionManager resolucion) {
     	
     	// 1. Aplica un retraso hasta que el componente esté dibujado y las coordenadas sean válidas.
-        SwingUtilities.invokeLater(() -> {
-    	
-        	Point puntoD = SwingUtilities.convertPoint(notaD, 0, 0, panelJuego);
-        	int posX = puntoD.x;
-        	
-    	
-        	// Crear una nota y agregarla al contentPane
-        	nota = new Nota(posX, 100, Color.RED, resolucion); // posición inicial y color
-        	panelJuego.add(nota); // Agrega la nota al fondo
 
-        	panelJuego.revalidate(); // Necesario para redibujar la nota
-        	
-        	// Timer para mover la nota
-        	int velocidad = resolucion.escalarY(5); // ajusta el valor base según lo que se vea fluido
-        	Timer timer = new Timer(10, new ActionListener() {
-        		public void actionPerformed(ActionEvent e) {
-        			nota.setLocation(nota.getX(), nota.getY() + velocidad); // velocidad de caída
-        		}
-        	});
-        	timer.start();
-        });
+    	SwingUtilities.invokeLater(() -> {
+    		SwingUtilities.invokeLater(() -> {
+
+
+    			Point puntoD = SwingUtilities.convertPoint(notaD, 0, 0, panelJuego);
+    			int posX = puntoD.x;
+
+    			// Crear una nota y agregarla al contentPane
+    			nota = new Nota(posX, 100, Color.RED, resolucion); // posición inicial y color
+    			panelJuego.add(nota); // Agrega la nota al fondo
+
+    			panelJuego.revalidate(); // Necesario para redibujar la nota
+
+
+    			// Timer para mover la nota
+    			int velocidad = resolucion.escalarY(5); // ajusta el valor base según lo que se vea fluido
+    			Timer timer = new Timer(10, new ActionListener() {
+    				public void actionPerformed(ActionEvent e) {
+    					nota.setLocation(nota.getX(), nota.getY() + velocidad); // velocidad de caída
+    				}
+
+    			});
+
+    			timer.start();
+    		});
+    	});
     }
 }
