8bdc566a77ec2c17b97abb62bab605140c3c1d39
Eluney Laje <eluneylaje@gmail.com>
2025-10-08 16:56:30 -0300

    se agrego lo necesario para que puedan hacer el mapeo

--- src/GUI/Cancion1.java
+++ src/GUI/Cancion1.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.SwingUtilities;
 import javax.swing.Timer;
@@ -14,7 +15,8 @@ public class Cancion1 extends CancionBase {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private Nota nota;
+
+	private ArrayList<Nota> notas;
 
     public Cancion1(ResolucionManager resolucion) {
         super(resolucion); // Llama al constructor de CancionBase
@@ -28,15 +30,38 @@ public class Cancion1 extends CancionBase {
 
     	SwingUtilities.invokeLater(() -> {
     		SwingUtilities.invokeLater(() -> {
-
+    			this.notas = new ArrayList<>();
+    			
 
     			Point puntoD = SwingUtilities.convertPoint(notaD, 0, 0, panelJuego);
-    			int posX = puntoD.x;
+    			int posX_D = puntoD.x;
+    			
+    			Point puntoF = SwingUtilities.convertPoint(notaF, 0, 0, panelJuego);
+    			int posX_F = puntoF.x;
+    			
+    			Point puntoJ = SwingUtilities.convertPoint(notaJ, 0, 0, panelJuego);
+    			int posX_J = puntoJ.x;
+    			
+    			Point puntoK = SwingUtilities.convertPoint(notaK, 0, 0, panelJuego);
+    			int posX_K = puntoK.x;
 
     			// Crear una nota y agregarla al contentPane
-    			nota = new Nota(posX, 100, Color.RED, resolucion); // posición inicial y color
-    			panelJuego.add(nota); // Agrega la nota al fondo
-
+    			Nota nota1 = new Nota(posX_D, 100, Color.RED, resolucion); // posición inicial y color
+    			panelJuego.add(nota1); // Agrega la nota al fondo
+    			this.notas.add(nota1);
+    			
+    			Nota nota2 = new Nota(posX_F, 100, Color.YELLOW, resolucion); // posición inicial y color
+    			panelJuego.add(nota2); // Agrega la nota al fondo
+    			this.notas.add(nota2);
+    			
+    			Nota nota3 = new Nota(posX_J, 100, Color.MAGENTA, resolucion); // posición inicial y color
+    			panelJuego.add(nota3); // Agrega la nota al fondo
+    			this.notas.add(nota3);
+    			
+    			Nota nota4 = new Nota(posX_K, 100, Color.GREEN, resolucion); // posición inicial y color
+    			panelJuego.add(nota4); // Agrega la nota al fondo
+    			this.notas.add(nota4);
+    			
     			panelJuego.revalidate(); // Necesario para redibujar la nota
 
 
@@ -44,7 +69,9 @@ public class Cancion1 extends CancionBase {
     			int velocidad = resolucion.escalarY(5); // ajusta el valor base según lo que se vea fluido
     			Timer timer = new Timer(10, new ActionListener() {
     				public void actionPerformed(ActionEvent e) {
+    					for (Nota nota : notas) {
     					nota.setLocation(nota.getX(), nota.getY() + velocidad); // velocidad de caída
+    					}
     				}
 
     			});
