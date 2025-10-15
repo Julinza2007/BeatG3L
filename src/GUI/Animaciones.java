c82ae74badd8d4f00a9dab4d4c73f0afed50c58c
gimeneznacho07 <136719918+gimeneznacho07@users.noreply.github.com>
2025-10-15 09:58:22 -0300

    Add files via upload

--- src/GUI/Animaciones.java
+++ src/GUI/Animaciones.java
@@ -38,10 +38,34 @@ public class Animaciones {
         return timer;
     }
     
-    public void btnHover() {
-    	
-    }
+ public static void zoomPantalla (JLabel logoLabel, ImageIcon logoBase) {
+	 final int duracion = 4000;
+	 final int delay = 30;
+	 final int pasos = duracion / delay;
+	 final int baseWidth = logoBase.getIconWidth();
+     final int baseHeight = logoBase.getIconHeight();
+     final double maxFactor = 1.40; // cuanto se agranda el logo
+     
+     final int[] pasoAct = {0}; 
+     
+     Timer timer = new Timer (delay, e -> {
+    	 double t = (double) pasoAct[0] / pasos;
+         double factor = 1.0 + (maxFactor - 1.0) * Math.sin(t * Math.PI / 2); 
+         int newWidth = (int) (baseWidth * factor);
+         int newHeight = (int) (baseHeight * factor);
+         
+         Image img = logoBase.getImage().getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+         logoLabel.setIcon(new ImageIcon(img));
+
+         pasoAct[0]++;
+         if (pasoAct[0] >= pasos) ((Timer) e.getSource()).stop();
+     });
+     
+     timer.start();
+ }
     
-    
+ public void btnHover() {
+ 	
+ }
     
 }
